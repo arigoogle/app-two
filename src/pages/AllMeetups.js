93b51a4ff66321ac04fb254efd931b8ee1f6c398
arigoogle <ariyantomail@gmail.com>
2023-02-06 16:24:23 +0700

    update

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -22,11 +23,45 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://react-getting-started-216a8-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json').then(response => {
+            return response.json();
+        }).then(data => {
+            const meetups = [];
+
+            for (const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        });
+    }, []);
+
+
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+
+        );
+    }
 
     return (
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetup={DUMMY_DATA} />
+            <MeetupList meetup={loadedMeetups} />
 
         </section>
     )
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,8 +1,33 @@
+import { json } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+
+        fetch
+            (
+                'https://react-getting-started-216a8-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+                {
+                    method: 'POST',
+                    body: JSON.stringify(meetupData),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                }
+            ).then(() => {
+                navigate('/');
+            });
+
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <form action=""></form>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+
         </section>
     )
 }
